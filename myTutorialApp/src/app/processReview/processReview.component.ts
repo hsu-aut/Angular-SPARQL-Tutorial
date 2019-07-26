53e1b3f4a9fa626120bcbde4f8cc9acc93aa0b9c
ConstantinHildebrandt <c.hildebrandt@hsu-hh.de>
2019-07-26 09:26:39 +0200

    applied master to 2-GraphDB-Data-Service

--- myTutorialApp/src/app/processReview/processReview.component.ts
+++ myTutorialApp/src/app/processReview/processReview.component.ts
@@ -31,74 +31,32 @@ export class ProcessReviewComponent implements OnInit {
   message: string;
 
   constructor(
-    private dataService: RdfDataService
+    // inject the service RdfDataService here
   ) { }
 
   ngOnInit() {
-    this.dataService.getMachines().subscribe((data: any) => {
-      this.machines = data;
-    });
+    // load the machine from the getMachines() service, assign data to machines
   }
 
   getOrderNumber(selectedMachine: string) {
     if (selectedMachine) {
-      this.dataService.getOrders(selectedMachine).subscribe((data: any) => {
-        this.orderNumbers = data;
-      });
+      // load orders from the getOrders(selectedMachine) service, assign data to orderNumbers
     }
   }
 
   getProcesses(selectedMachine: string, selectOrderNumber: string) {
     if (selectedMachine && selectOrderNumber) {
-      this.dataService.getProcesses(selectedMachine, selectOrderNumber).subscribe((data: any) => {
-        this.processes = data;
-      });
+      // load processes from the getProcesses(selectedMachine, selectOrderNumber) service, assign data to processes
     }
   }
 
   getProcessData(selectedProcess: string) {
     if (selectedProcess) {
       this._loaderShow = true;
-      this.dataService.getProcessData(selectedProcess).subscribe((data: any) => {
-        this._loaderShow = false;
-        if (data.length != 0) {
-          this.processDataTable = data;
-          // this.processDataTable = this.checkData(this.processDataTable);
-          console.log(this.processDataTable)
-          this.message = undefined;
-        } else {
-          this.message = "There is no process data available.";
-        }
-      });
+      // load process data from the getProcessData(selectedProcess) service, assign data to the processDataTable
+      // set this._loaderShow to false as soon as the data is available
+      // set this.message to undefined in case that there is data
+      // set a senseful this.message in case that there is no data
     }
-  }
-
-  checkData(table) {
-    for (let row = 0; row < table.length; row++) {
-      let nominalTemp = table[row].NominalTemperatureHC1Value;
-      let actualTemp = table[row].ActualTemperatureHC1Value;
-      let nominalPressure = table[row].NominalPressureValue;
-      let actualPressure = table[row].ActualPressureValue;
-
-      if (nominalTemp == 0 || actualTemp == 0) {
-        table[row].TemperatureCheck = '-';
-      } else if (Math.abs(1 - (nominalTemp / actualTemp)) * 100 <= this.valueOK) {
-        table[row].TemperatureCheck = 'ok';
-      } else if (Math.abs(1 - (nominalTemp / actualTemp)) * 100 <= this.valueWarning) {
-        table[row].TemperatureCheck = 'warning';
-      } else {
-        table[row].TemperatureCheck = 'alert';
-      }
-      if (nominalPressure == 0 || actualPressure == 0) {
-        table[row].PressureCheck = '-';
-      } else if (Math.abs(1 - (nominalPressure / actualPressure)) * 100 <= this.valueOK) {
-        table[row].PressureCheck = 'ok';
-      } else if (Math.abs(1 - (nominalPressure / actualPressure)) * 100 <= this.valueWarning) {
-        table[row].PressureCheck = 'warning';
-      } else {
-        table[row].PressureCheck = 'alert';
-      }
-    }
-    return table;
   }
 }
